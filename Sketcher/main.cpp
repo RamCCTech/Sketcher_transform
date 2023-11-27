fa7aedfcea9bfa74d0a7807a99b4a63b2c199bdf
RamCCTech <149322355+RamCCTech@users.noreply.github.com>
2023-11-27 17:39:10 +0530

    Add files via upload

---
+++ Sketcher/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include "headers/Reader.h"
+#include "headers/Triangle.h"
+#include "headers/Triangulation.h"
+#include "headers/Writer.h"
+#include "headers/Transform.h"
+
+int main()
+{
+    std::vector<Triangle> triangles;
+    Reader reader("resources/cubeModel.stl");
+    reader.getTriangles(triangles);
+
+    Triangulation triangulation(triangles);
+
+    Writer writer;
+
+    std::vector<Triangle> outTriangles=triangulation.getTriangles();
+
+    Transform transform(outTriangles);
+    transform.scale(2);
+
+    writer.write("output/output.txt", outTriangles);
+
+}
